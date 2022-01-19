f7c3640c5d11add986c3d537a73aa448d24fec05
Michal Vala <michal.vala@gmail.com>
2022-01-19 21:21:28 +0100

    matches list as table

--- assets/list.js
+++ assets/list.js
@@ -10,17 +10,23 @@ const reqParamHeaders = {
 }
 
 function matchRow(key, TeamA, TeamB, ScoreA, ScoreB, time) {
-    return <li className="w3-row w3-blue-gray" key={key}>
-        <div className="w3-col l2">
+    return <tr key={key}>
+        <td className="w3-col l2">
             {time}
-        </div>
-        <div className="w3-col l3">
+        </td>
+        <td className="w3-col l1">
             de_dust2
-        </div>
-        <div className="w3-col l6">
+        </td>
+        <td className="w3-col l1">
+            5v5
+        </td>
+        <td className="w3-col l6">
             <span className={ScoreA > ScoreB ? "w3-green" : ""}>{TeamA}</span> {ScoreA} : {ScoreB} <span className={ScoreA < ScoreB ? "w3-green" : ""}>{TeamB}</span>
-        </div>
-    </li>;
+        </td>
+        <td className="w3-col l2">
+            <a href="#">Faceit</a> <a href="#">2D</a>
+        </td>
+    </tr>;
 }
 
 fetch(`${faceitApiUrlBase}/players/${sprPlayerId}/history`, reqParamHeaders)
@@ -32,7 +38,8 @@ function handleMatches(matchesResponse) {
     const matchesList = []
     matchesResponse.items.forEach(match => {
         const time = new Date(match.finished_at * 1000);
-        matchesList.push(matchRow(match.match_id, match.teams.faction1.nickname, match.teams.faction2.nickname, match.results.score.faction1, match.results.score.faction2, time.toISOString()))
+        const dateString = `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()} ${time.getHours()}:${time.getMinutes()}`
+        matchesList.push(matchRow(match.match_id, match.teams.faction1.nickname, match.teams.faction2.nickname, match.results.score.faction1, match.results.score.faction2, dateString))
     })
     ReactDOM.render(
         matchesList,
