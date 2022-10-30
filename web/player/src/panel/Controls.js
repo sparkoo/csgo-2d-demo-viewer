968712ccddd9d77663cd6ded5ce0efa3d979f3b5
Michal Vala <michal.vala@gmail.com>
2022-10-30 19:36:58 +0100

    play faster button <3 beggi

--- web/player/src/panel/Controls.js
+++ web/player/src/panel/Controls.js
@@ -1,12 +1,13 @@
 import "./Controls.css"
-import {Component} from "react";
-import {MSG_PLAY_CHANGE, MSG_PLAY_ROUND_INCREMENT, MSG_PLAY_SPEED, MSG_PLAY_TOGGLE} from "../constants";
+import { Component } from "react";
+import { MSG_PLAY_CHANGE, MSG_PLAY_ROUND_INCREMENT, MSG_PLAY_SPEED, MSG_PLAY_TOGGLE } from "../constants";
 
 class Controls extends Component {
   constructor(props) {
     super(props);
     this.state = {
       playing: false,
+      playingSpeed: 1,
     }
     this.messageBus = props.messageBus
     this.messageBus.listen([MSG_PLAY_CHANGE], function (msg) {
@@ -30,41 +31,52 @@ class Controls extends Component {
   }
 
   playSpeed(speed) {
+    this.setState({
+      playingSpeed: speed
+    })
     this.messageBus.emit({
       msgtype: MSG_PLAY_SPEED,
       speed: speed,
     })
   }
 
+  togglePlaySpeed(speed) {
+    if (this.state.playingSpeed === speed) {
+      this.playSpeed(1)
+    } else {
+      this.playSpeed(speed)
+    }
+  }
+
   render() {
     const playButton = this.state.playing ? String.fromCodePoint(0xe034) : String.fromCodePoint(0xe037)
 
     return (
-        <div className="w3-row">
-          <div className="w3-col l4">&nbsp;</div>
-          <div className="w3-col l4">
-            <button className="w3-button w3-ripple w3-dark-gray material-icons"
-                    onClick={_ => this.playRoundIncrement(-1)}>&#xe045;
-            </button>
-            <button className="w3-button w3-ripple w3-dark-gray material-icons"
-                    onClick={_ => this.togglePlay()}>{playButton}
-            </button>
-            <button className="w3-button w3-ripple w3-dark-gray material-icons"
-                    onClick={_ => this.playRoundIncrement(1)}>&#xe044;
-            </button>
-          </div>
-          {/*<div className="w3-col l4 w3-right-align">*/}
-          {/*  <button className="w3-button w3-ripple w3-dark-gray material-icons" onClick={_ => this.playSpeed(.5)}>*/}
-          {/*    &#xe068;*/}
-          {/*  </button>*/}
-          {/*  <button className="w3-button w3-ripple w3-dark-gray material-icons" onClick={_ => this.playSpeed(1)}>*/}
-          {/*    &#xe037;*/}
-          {/*  </button>*/}
-          {/*  <button className="w3-button w3-ripple w3-dark-gray material-icons" onClick={_ => this.playSpeed(4)}>*/}
-          {/*    &#xe01f;*/}
-          {/*  </button>*/}
-          {/*</div>*/}
+      <div className="w3-row">
+        <div className="w3-col l4">&nbsp;</div>
+        <div className="w3-col l4">
+          <button className="w3-button w3-ripple w3-dark-gray material-icons"
+            onClick={_ => this.playRoundIncrement(-1)}>&#xe045;
+          </button>
+          <button className="w3-button w3-ripple w3-dark-gray material-icons"
+            onClick={_ => this.togglePlay()}>{playButton}
+          </button>
+          <button className="w3-button w3-ripple w3-dark-gray material-icons"
+            onClick={_ => this.playRoundIncrement(1)}>&#xe044;
+          </button>
         </div>
+        <div className="w3-col l4 w3-right-align">
+          {/* <button className="w3-button w3-ripple w3-dark-gray material-icons" onClick={_ => this.playSpeed(.5)}>
+            &#xe068;
+          </button>
+          <button className="w3-button w3-ripple w3-dark-gray material-icons" onClick={_ => this.playSpeed(1)}>
+            &#xe037;
+          </button> */}
+          <button className={`w3-button w3-ripple ${this.state.playingSpeed === 4 ? "w3-pale-red w3-hover-pink" : "w3-dark-grey"}`} onClick={_ => this.togglePlaySpeed(4)}>
+            beggi
+          </button>
+        </div>
+      </div>
     )
   }
 }
