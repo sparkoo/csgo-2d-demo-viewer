9a613e4d940815f71381e10e9c136b129fafe1da
Michal Vala <michal.vala@gmail.com>
2023-11-12 14:00:32 +0100

    cs2 WIP

--- web/index/src/App.js
+++ web/index/src/App.js
@@ -1,99 +1,19 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import MatchTable from './MatchTable/MatchTable';
 
 function App() {
-  const [auth, setAuth] = useState([]);
-  const [serverHost] = useState(window.location.host.includes("localhost") ? "http://localhost:8080" : "")
-  const [content, setContent] = useState([]);
-
-  useEffect(() => {
-    if (Object.keys(auth).length > 0) {
-      setContent(<MatchTable auth={auth} serverHost={serverHost} />)
-      return
-    }
-    setContent(<span className="material-icons w3-xxxlarge rotate">autorenew</span>)
-
-    fetch(serverHost + "/auth/whoami", { credentials: "include" })
-      .then(response => response.json())
-      .then(data => {
-        if (Object.keys(data).length > 0) {
-          setAuth(data)
-        } else {
-          setContent(<span className="w3-xxxlarge rotate">Connect account first</span>)
-        }
-      })
-      .catch(err => {
-        console.log(err)
-        setContent(<span className="w3-xxxlarge rotate">failed to contact server ...</span>)
-      })
-  }, [serverHost, auth])
-
-  let faceitAuth = (
-    <div className='faceitAuth'>
-      <a href={serverHost + "/auth/faceit/login"}>
-        Connect FACEIT <img src="https://upload.wikimedia.org/wikipedia/commons/5/52/Cib-faceit_%28CoreUI_Icons_v1.0.0%29.svg" height="32" alt="faceit-logo" />
-      </a>
-    </div>)
-  let steamAuth = (
-    <div className='steamAuth'>
-      <a href={serverHost + "/auth/steam/login"}>
-        Connect Steam <img src='https://upload.wikimedia.org/wikipedia/commons/8/83/Steam_icon_logo.svg' height="32" alt="steam-login" />
-      </a>
-    </div>
-  )
-  if (Object.keys(auth).length > 0) {
-    if (auth.faceitNickname) {
-      faceitAuth = (
-        <div className='faceitAuth'>
-          <a href={"https://www.faceit.com/en/players/" + auth.faceitNickname} target="_blank" rel="noreferrer">
-            <img src="/assets/faceit-logo.svg" alt="faceit-logo" height="32" /><span id="faceitNickname">{auth.faceitNickname}</span>
-          </a>
-          <a className="material-icons w3-large" href={serverHost + "/auth/faceit/logout"}>logout</a>
-        </div>
-      )
-    }
-    if (auth.steamId) {
-      steamAuth = (
-        <div className='steamAuth'>
-          <a href={"https://steamcommunity.com/profiles/" + auth.steamId} target="_blank" rel="noreferrer">
-          <img src={auth.steamAvatar} height="32" alt="steam-login" /><span id="steamNickname">{auth.steamUsername}</span>
-          </a>
-          <a className="material-icons w3-large" href={serverHost + "/auth/steam/logout"}>logout</a>
-        </div>
-      )
-    }
-  }
-
   return (
-    <div className="App">
-      <div className="w3-container">
-        <div className="w3-row">
-          <div className="w3-col l2">
-            <br />
-          </div>
-          <div className="w3-col l8">
-            <div className="w3-container w3-xlarge w3-light-grey">
-              <div className="w3-row">
-                <div className="w3-col l4 w3-left-align">
-                  <a href="/"><h1>2d.sparko.cz</h1></a>
-                </div>
-                <div className='w3-col l4'>
-                  &nbsp;
-                </div>
-                <div className="w3-col l4 w3-right-align">
-                  <div className='faceit'>{faceitAuth}</div>
-                  <div className='steam'>{steamAuth}</div>
-                </div>
-              </div>
-            </div>
-            <div id="searchNote" className="w3-margin w3-container w3-center loader w3-xlarge">
-            </div>
-            {content}
-          </div>
-          <div className="w3-col l2">
-            <br />
-          </div>
+    <div className="w3-container w3-margin-top">
+      <div className="w3-row w3-center">
+        <div className="w3-quarter">
+          &nbsp;
+        </div>
+        <div className="w3-half w3-display-container w3-cell-middle">
+          <img
+            src="https://media3.giphy.com/media/4VRgyzM90w0SoT5BV9/giphy.gif?cid=ecf05e47dnwcxzy13u09zyd37843vdgifb9d5bf9d467crti"
+            className="w3-circle w3-border-blue-gray w3-hover-border-gray w3-padding w3-leftbar w3-rightbar" alt="cs2" style={{width:'100%'}} />
+        </div>
+        <div className="w3-quarter">
+          &nbsp;
         </div>
       </div>
     </div>
