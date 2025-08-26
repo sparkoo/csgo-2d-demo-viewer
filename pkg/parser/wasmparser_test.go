3618e797837a6ce4509c8dabace5cf56fcee76f4
Michal Vala <michal.vala@gmail.com>
2025-08-26 12:58:13 +0200

    fix uninitialized logger in tests (#84)

--- pkg/parser/wasmparser_test.go
+++ pkg/parser/wasmparser_test.go
@@ -5,7 +5,8 @@ import (
 	"testing"
 )
 
-var zstDemofileName = "1-cde451fd-6cd4-4c87-a432-d97d2235021a-1-1.dem.zst"
+// var zstDemofileName = "1-cde451fd-6cd4-4c87-a432-d97d2235021a-1-1.dem.zst"
+var zstDemofileName = "1-5264a308-d0ce-4f3b-ac1b-b3e3c7bd6c3a-1-1.dem.zst"
 var gzDemofileName = "1-cde451fd-6cd4-4c87-a432-d97d2235021a-1-1.dem.gz"
 var testDemosFolderPath = "../../testdemos"
 
