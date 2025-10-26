d0b84bea3ebf3bc05708580666f913f6ddb337b0
Michal Vala <michal.vala@gmail.com>
2025-10-26 08:38:52 +0100

    homepage cleanup

--- web/src/Index/Home.jsx
+++ web/src/Index/Home.jsx
@@ -1,17 +1,9 @@
-import { useEffect, useState } from 'react';
-import './Home.css';
-import MatchTable from './MatchTable/MatchTable';
-import DemoLinkInput from './DemoLinkInput/DemoLinkInput';
-import Uploader from './Uploader/Uploader';
+import { useState } from "react";
+import "./Home.css";
+import DemoLinkInput from "./DemoLinkInput/DemoLinkInput";
+import Uploader from "./Uploader/Uploader";
 
 export function Home() {
-  const [auth/*, setAuth*/] = useState([]);
-  const [serverHost] = useState(window.location.host.includes("localhost") ? "http://localhost:8080" : "");
-  const [content, setContent] = useState([]);
-
-  const [isWasmLoaded, setIsWasmLoaded] = useState(false);
-
-
   return (
     <div className="App">
       <div className="w3-container">
@@ -23,20 +15,18 @@ export function Home() {
             <div className="w3-container w3-xlarge w3-light-grey">
               <div className="w3-row">
                 <div className="w3-col l4 w3-left-align">
-                  <a href="/"><h1>2d.sparko.cz</h1></a>
+                  <a href="/">
+                    <h1>2d.sparko.cz</h1>
+                  </a>
                 </div>
-                <div className='w3-col l4'>
-                  &nbsp;
-                </div>
-                <div className="w3-col l4 w3-right-align">
-                  {/* <div className='faceit'>{faceitAuth}</div> */}
-                  {/* <div className='steam'>{steamAuth}</div> */}
-                </div>
+                <div className="w3-col l4">&nbsp;</div>
+                <div className="w3-col l4 w3-right-align"></div>
               </div>
             </div>
-            <div id="searchNote" className="w3-margin w3-container w3-center loader w3-xlarge">
-            </div>
-            {content}
+            <div
+              id="searchNote"
+              className="w3-margin w3-container w3-center loader w3-xlarge"
+            ></div>
             <DemoLinkInput />
             <Uploader />
             <hr />
